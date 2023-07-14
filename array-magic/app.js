83c6cc86019b24829cdc9bc1723e9bb4574a9bae
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-14 11:41:53 +0300

    arr

---
+++ array-magic/app.js
@@ -0,0 +1,38 @@
+'use strict';
+console.log('app.js file was loaded');
+
+const mainArr = [
+  {
+    name: 'Mickey Mouse',
+    age: 92,
+    gender: 'Male',
+    town: 'Disneyville',
+  },
+  {
+    name: 'SpongeBob SquarePants',
+    age: 33,
+    gender: 'Male',
+    town: 'Bikini Bottom',
+  },
+  {
+    name: 'Dora the Explorer',
+    age: 14,
+    gender: 'Female',
+    town: 'Playa Verde',
+  },
+  {
+    name: 'Bugs Bunny',
+    age: 83,
+    gender: 'Male',
+    town: 'Looneyville',
+  },
+  {
+    name: 'Lisa Simpson',
+    age: 9,
+    gender: 'Female',
+    town: 'Springfield',
+  },
+];
+
+// console.log('mainArr ===', mainArr);
+console.table(mainArr);
