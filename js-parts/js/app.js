0286e7e7ad2ed762cd92189472117125dbfd728c
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-14 10:40:27 +0300

    uzd

--- js-parts/js/app.js
+++ js-parts/js/app.js
@@ -1,9 +1,10 @@
-console.log('app.js file was loaded');
 // named exporto importas
 import { username, age } from './vars.js';
 import { sum } from './modules/functions.js';
 import { printToHtml } from './modules/html.js';
+import { town, avgArr } from './modules/variables.js';
 // iskonsolinti username ir age esanti vars.js
+console.log('app.js file was loaded');
 console.log('username ===', username);
 console.log('age ===', age);
 // const age = 65;
@@ -14,3 +15,7 @@ const rezult = sum(111, 222);
 printToHtml(rezult);
 printToHtml('labukas');
 printToHtml('Man jau labai neblogai eina javascripts');
+
+console.log('town ===', town);
+printToHtml('lietuvos sirdis: ' + town);
+console.log('avgArr ===', avgArr);
---
+++ js-parts/js/modules/variables.js
@@ -0,0 +1,4 @@
+console.log('variables.js file was loaded');
+
+export const town = 'Kaunas';
+export const avgArr = [4.5, 3.2, 5.1, 2.7, 4.8];
